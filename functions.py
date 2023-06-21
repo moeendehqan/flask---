2cd7a87fe1e2a4e03d6a5ffb4a3c81fc409a4e29
moeen dehqan <moeen.dehqan@gmail.com>
2023-06-21 12:37:49 +0330

    sdfsdfs

--- functions.py
+++ functions.py
@@ -4,4 +4,4 @@ import json
 
 def getConfig(data):
     id = data['id']
-    return json.dumps({'replay':True,'config':{'ic':'556464654','mc':'564564564655456454644','wf':'44545456456465'}})
+    return json.dumps({'replay':True,'config':{'ic':'556464654','mc':'564564564655456454644','wf':'44545456456465',}})
